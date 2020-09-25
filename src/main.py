4995fca2ea5a5166beced1b0c6fef3e71d035014
Deveaux Benoît <contact.flapili@gmail.com>
2020-09-25 15:33:47 +0200

    add explanation about linux host and venv struct

--- src/main.py
+++ src/main.py
@@ -29,7 +29,8 @@ async def upload_src(env_name: str, file: UploadFile = File(...)):
     # install requirements
     req = p / "src" / "requirements.txt"
     if req.is_file():
-        process = subprocess.Popen([p / "venv" / "Scripts" / "pip.exe", "install", "-r", req])
+        # replace Script/pip.exe by bin/pip on linux
+        process = subprocess.Popen([p / "venv" / "Scripts" / "pip.exe", "install", "-r", req])  
         process.wait()
 
     return f"env {env_name} created"
@@ -38,6 +39,7 @@ async def upload_src(env_name: str, file: UploadFile = File(...)):
 @app.get("/run_env")
 async def run_env(env_name: str, script_name: str):
     p = Path("envs") / env_name
+    # replace Script/python.exe by bin/python on linux
     process = subprocess.Popen([p / "venv" / "Scripts" / "python.exe", script_name], stdout=subprocess.PIPE)
     output, _ = process.communicate()
 
