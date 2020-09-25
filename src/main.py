c8c474638c190bf464cb10866a4838e7ffb0deb0
Deveaux Benoît <contact.flapili@gmail.com>
2020-09-25 15:31:23 +0200

    add main file

---
+++ src/main.py
@@ -0,0 +1,44 @@
+# coding: utf-8
+import io
+from pathlib import Path
+import zipfile
+import venv
+import subprocess
+
+from fastapi import FastAPI, UploadFile, File
+
+
+app = FastAPI()
+
+
+@app.post("/upload_src/{env_name}")
+async def upload_src(env_name: str, file: UploadFile = File(...)):
+
+    # read the zip archive
+    content = await file.read()
+
+    # extract in temp folder
+    p = Path("envs") / env_name
+    p.mkdir(parents=True)
+    with zipfile.ZipFile(io.BytesIO(content), 'r') as zip:
+        zip.extractall(path=p / "src")
+
+    # create a venv with pip
+    venv.EnvBuilder(with_pip=True).create(p / "venv")
+
+    # install requirements
+    req = p / "src" / "requirements.txt"
+    if req.is_file():
+        process = subprocess.Popen([p / "venv" / "Scripts" / "pip.exe", "install", "-r", req])
+        process.wait()
+
+    return f"env {env_name} created"
+
+
+@app.get("/run_env")
+async def run_env(env_name: str, script_name: str):
+    p = Path("envs") / env_name
+    process = subprocess.Popen([p / "venv" / "Scripts" / "python.exe", script_name], stdout=subprocess.PIPE)
+    output, _ = process.communicate()
+
+    return output
